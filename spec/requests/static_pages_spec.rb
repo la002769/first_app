eaff971c2d1d8136906af3fdc68af0b89e267fae
alexia <la002769@etu.unice.fr>
2012-11-28 21:20:12 +0100

    Tests plus beaux toutes les pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,64 +11,56 @@ subject { page }
 	                        text: "Ruby on Rails Tutorial Example App" }
 	it { should_not have_selector 'title', text: '|Home' }
 	  end
-describe "Help page" do
- it "should have the h1 'Example App'" do
-      visit help_path
-      page.should have_selector('h1', :text => 'Example App')
-    end
 
-    it "should have the title 'Help'" do
-      visit help_path
-      page.should have_selector('title',
-          :text => "Ruby on Rails Tutorial Example App | Help")
-  end
-end
-describe "About page" do
- it "should have the h1 'Example App'" do
-      visit about_path
-      page.should have_selector('h1', :text => 'Example App')
-    end
+subject { page }
 
-    it "should have the title 'About'" do
-      visit about_path
-      page.should have_selector('title',
-          :text => "Ruby on Rails Tutorial Example App | About")
-  end
-end
-describe "Contact page" do
- it "should have the h1 'Example App'" do
-      visit contact_path
-      page.should have_selector('h1', :text => 'Example App')
-    end
+	  describe "Help page" do
+	    before { visit help_path } 
 
-    it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title',
-          :text => "Ruby on Rails Tutorial Example App | Contact")
-  end
+	    it { should have_selector('h1', text: 'Example App') }
+	    it { should have_selector 'title',
+	                        text: "Ruby on Rails Tutorial Example App | Help" }
 end
-describe "Signin page" do
- it "should have the h1 'Example App'" do
-      visit signin_path
-      page.should have_selector('h1', :text => 'Example App')
-    end
 
-    it "should have the title 'Sign in'" do
-      visit signin_path
-      page.should have_selector('title',
-          :text => "Ruby on Rails Tutorial Example App | Sign in")
-  end
-end
-describe "Signup page" do
- it "should have the h1 'Example App'" do
-      visit signup_path
-      page.should have_selector('h1', :text => 'Example App')
-    end
+subject { page }
 
-    it "should have the title 'Sign up'" do
-      visit signup_path
-      page.should have_selector('title',
-          :text => "Ruby on Rails Tutorial Example App | Sign up")
-  end
+	  describe "About page" do
+	    before { visit about_path } 
+
+	    it { should have_selector('h1', text: 'Example App') }
+	    it { should have_selector 'title',
+	                        text: "Ruby on Rails Tutorial Example App | About" }
+end
+
+subject { page }
+
+	  describe "Contact page" do
+	    before { visit contact_path } 
+
+	    it { should have_selector('h1', text: 'Example App') }
+	    it { should have_selector 'title',
+	                        text: "Ruby on Rails Tutorial Example App | Contact" }
+end
+
+subject { page }
+
+	  describe "Signin page" do
+	    before { visit signin_path } 
+
+	    it { should have_selector('h1', text: 'Example App') }
+	    it { should have_selector 'title',
+	                        text: "Ruby on Rails Tutorial Example App | Sign in" }
+end
+
+subject { page }
+
+	  describe "Signup page" do
+	    before { visit signup_path } 
+
+	    it { should have_selector('h1', text: 'Example App') }
+	    it { should have_selector 'title',
+	                        text: "Ruby on Rails Tutorial Example App | Sign up" }
 end
 end
+
+
