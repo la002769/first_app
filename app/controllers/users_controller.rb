731cde07bbfb8e8ffe26a3ea30538b4dde8e72ff
alexia <la002769@etu.unice.fr>
2012-12-06 00:58:26 +0100

    Bon utilisateur

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
  before_filter :signed_in_user, only: [:edit, :update]
+ before_filter :correct_user,   only: [:edit, :update]
  
 def show
 	@user = User.find(params[:id])
@@ -19,10 +20,8 @@ sign_in @user
   end
   end
 def edit
-    @user = User.find(params[:id])
   end
  def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
@@ -35,5 +34,9 @@ def edit
 
     def signed_in_user
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 end
