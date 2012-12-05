039b808f6374456a4919ce322c4e29c5528890b2
alexia <la002769@etu.unice.fr>
2012-12-05 23:13:04 +0100

    Sign in out

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -34,13 +34,15 @@ subject { page }
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
     end
   end
- # subject { page }
-#	describe "signup page" do
-#		before { visit signup_path }
-	
-#		it { should have_selector('h1', text: 'Sign up') }
-#		it { should have_selector('title', text: full_title('Sign up')) }
-#    end
+
   end
