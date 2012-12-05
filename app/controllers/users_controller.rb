0dc85c750b3e90babaa2772e9d37405935bf749b
alexia <la002769@etu.unice.fr>
2012-12-06 00:47:15 +0100

    Etre connecte pour modif info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  
+
+ before_filter :signed_in_user, only: [:edit, :update]
+ 
 def show
 	@user = User.find(params[:id])
 end
@@ -29,4 +31,9 @@ def edit
       render 'edit'
     end
   end
+ private
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -47,6 +47,10 @@ describe "after saving the user" do
 
 describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
+ before do
+      sign_in user
+      visit edit_user_path(user)
+    end
     before { visit edit_user_path(user) }
 
     describe "page" do
