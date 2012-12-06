6a3587e3c4f508e0489f3065fff11e6abeabaad2
alexia <la002769@etu.unice.fr>
2012-12-06 23:07:18 +0100

    access control

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -32,5 +32,16 @@ def redirect_back_or(default)
     session[:return_to] = request.url
   end
 
+def current_user?(user)
+    user == current_user
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
 end
 
