ff424bdd3e6fb13407bac4d89fe95198ee4817f7
alexia <la002769@etu.unice.fr>
2012-12-06 01:14:36 +0100

    debut index tous les utilisateurs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
- before_filter :signed_in_user, only: [:edit, :update]
+ before_filter :signed_in_user, only: [:index, :edit, :update]
  before_filter :correct_user,   only: [:edit, :update]
  
 def show
@@ -32,6 +32,11 @@ def edit
       render 'edit'
     end
   end
+
+def index
+	@users = User.all
+end
+
  private
 
     def signed_in_user
