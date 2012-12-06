6d6aad0db43a9bf9c5a35751d9f86366f88d53d4
alexia <la002769@etu.unice.fr>
2012-12-06 23:40:49 +0100

    Feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,16 @@ validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniquenes
 validates :password, presence: true, length: { minimum: 6 }
 validates :password_confirmation, presence: true
 
+def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
+
 private
 
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64
     end
+
+
 end
