d29ed970ba73bee7dfa2936757211accf50c3004
alexia <la002769@etu.unice.fr>
2012-12-06 22:28:38 +0100

    Finish user edit, update, index and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,7 +8,7 @@ describe "index" do
 
     let(:user) { FactoryGirl.create(:user) }
 
-    before(:each) do
+   before do
       sign_in user
       visit users_path
     end
@@ -29,7 +29,27 @@ describe "index" do
         end
       end
     end
-  end
+
+describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end
+end
+
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
