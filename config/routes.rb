ad10e509aac6939810e5d9d7ed93cdc833617e34
alexia <la002769@etu.unice.fr>
2012-12-07 10:17:47 +0100

    Section 11.2.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 FirstApp::Application.routes.draw do
   get "users/new"
-
+resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 resources :users
- resources :sessions, only: [:new, :create, :destroy]
-resources :microposts, only: [:create, :destroy]
+ resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   root to: 'static_pages#home'
 
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -110,7 +110,19 @@ describe "as non-admin user" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
+
+describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
       end
+
     end
 	describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
