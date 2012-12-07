6082092f86add980398535a12cdfc6528fc2c6ce
alexia <la002769@etu.unice.fr>
2012-12-07 10:47:30 +0100

    Version de fin du TP11

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,8 +29,7 @@ validates :password, presence: true, length: { minimum: 6 }
 validates :password_confirmation, presence: true
 
 def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
 
 def following?(other_user)
