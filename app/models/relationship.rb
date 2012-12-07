835df5851dd4df98e79c1e89d9e9855cd477e032
alexia <la002769@etu.unice.fr>
2012-12-07 09:39:37 +0100

    Followed users

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,5 +3,7 @@ class Relationship < ActiveRecord::Base
 
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
-end
 
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
+end
