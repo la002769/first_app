7b970361b778b97e55046d6e0a682b5c148da0b0
alexia <la002769@etu.unice.fr>
2012-12-07 10:24:46 +0100

    A working follow button the standard way

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -69,6 +69,8 @@ describe "followed by signout" do
         end
       end
 
+
+
 describe "in the Microposts controller" do
 
         describe "submitting to the create action" do
@@ -122,6 +124,19 @@ describe "visiting the following page" do
         end
 
       end
+
+ describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_path) }          
+        end
+      end
+
 
     end
 	describe "as wrong user" do
